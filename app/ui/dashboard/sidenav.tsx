0aafc27f10480c2857be0046336f593d8db6b658
Jakub Kasprzyk <neus90@gmail.com>
2024-04-13 19:46:29 +0200

    Add scrolling the nav section #218

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -10,7 +10,7 @@ export default async function SideNav() {
   const { user } = await getSession();
 
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex h-full flex-col overflow-y-auto px-3 py-4 md:px-2">
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-white p-4 md:h-40"
         href="/"
@@ -21,7 +21,6 @@ export default async function SideNav() {
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks id={user.id} groupId={user.groupId} role={user.role} />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form
           action={async () => {
             'use server';
