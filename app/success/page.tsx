e08dbd6c22bc5eaab3d8616c8b5796653b0ec9ee
Jakub Kasprzyk <neus90@gmail.com>
2024-04-23 19:11:02 +0200

    Emails are not sent after group creation #254

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -14,6 +14,21 @@ export default function GroupCreateSuccessPage() {
       <a className="text-blue-600 underline" href="https://www.emeryk.pttk.pl/">
         https://www.emeryk.pttk.pl/
       </a>
+      <p className="mt-6">
+        <strong className="font-bold">
+          Każde zgłoszenie jest potwierdzane na adres e-mail podany w
+          zgłoszeniu.
+          <br /> W przypadku nieotrzymania wiadomości w ciągu 30 min od
+          prawidłowo wysłanego zgłoszenia prosimy o sprawdzenie folderu SPAM.
+          <br />W razie problemów prosimy o kontakt mailowy: &nbsp;
+          <a
+            className="text-blue-600 underline"
+            href="mailto:kontakt@emeryk.pttk.pl"
+          >
+            kontakt@emeryk.pttk.pl
+          </a>
+        </strong>
+      </p>
     </main>
   );
 }
