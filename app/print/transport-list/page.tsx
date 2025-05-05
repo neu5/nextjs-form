a557934b2564ca50dbf7f71e99fc764f63e645ac
jura19pl <167467696+jura19pl@users.noreply.github.com>
2025-05-05 06:20:03 +0200

    Update page.tsx

--- app/print/transport-list/page.tsx
+++ app/print/transport-list/page.tsx
@@ -1,1 +1,10 @@
+import { Metadata } from 'next';
+import Table from '@/app/ui/print/transport-list';
 
+export const metadata: Metadata = {
+  title: 'Do druku',
+};
+
+export default function Print() {
+  return <Table />;
+}
